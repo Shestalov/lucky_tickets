7eb57dc767a9d6b2c0e04d12f9fb7466abe37674
Alex <alexshestalov@gamil.com>
2022-09-09 13:11:26 +0300

    Multiprocessing Multithreading

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import threading
 import time
-from multiprocessing import Process, current_process
+from multiprocessing import Process, current_process, Queue
 import multiprocessing
 
 tickets_1 = ["000000", "250000"]
@@ -9,28 +9,42 @@ tickets_2 = ["250001", "500000"]
 tickets_3 = ["500001", "750000"]
 tickets_4 = ["750001", "999999"]
 
-total_tickets = 0
+# for th
+total_tickets_th = 0
 
+
+# # for mp
+# queue = Queue()
 
 def check_ticket(ticket):
     start = ticket[0]
     end = ticket[1]
     lucky = 0
 
-    # logging.info("Thread %s: starting", threading.currentThread().name)
-    print(f"{current_process().name} starting")
+    # for th
+    logging.info("Thread %s: starting", threading.currentThread().name)
+
+    # # for mp
+    # print(f"{current_process().name} starting")
 
     for num in range(int(start), int(end) + 1):
         i = str(num).rjust(6, "0")
         if (int(i[0]) + int(i[1]) + int(i[2])) == (int(i[3]) + int(i[4]) + int(i[5])):
             lucky += 1
 
-    # logging.info("Thread %s: finishing", threading.currentThread().name)
-    # print(f"{threading.currentThread().name} - {lucky} tickets")
-    print(f"{current_process().name} finishing - {lucky} tickets")
+    # for th
+    logging.info("Thread %s: finishing", threading.currentThread().name)
+    print(f"{threading.currentThread().name} - {lucky} tickets")
 
-    global total_tickets
-    total_tickets += lucky
+    # # for mp
+    # print(f"{current_process().name} finishing - {lucky} tickets")
+
+    # for th
+    global total_tickets_th
+    total_tickets_th += lucky
+
+    # # for mp
+    # queue.put(lucky)
 
 
 if __name__ == '__main__':
@@ -39,45 +53,52 @@ if __name__ == '__main__':
 
     logging.info("Main: before creating thread/process")
 
-    # thread1 = threading.Thread(target=check_ticket, args=(tickets_1,))
-    # thread2 = threading.Thread(target=check_ticket, args=(tickets_2,))
-    # thread3 = threading.Thread(target=check_ticket, args=(tickets_3,))
-    # thread4 = threading.Thread(target=check_ticket, args=(tickets_4,))
+    thread1 = threading.Thread(target=check_ticket, args=(tickets_1,))
+    thread2 = threading.Thread(target=check_ticket, args=(tickets_2,))
+    thread3 = threading.Thread(target=check_ticket, args=(tickets_3,))
+    thread4 = threading.Thread(target=check_ticket, args=(tickets_4,))
 
-    process1 = Process(target=check_ticket, args=(tickets_1,))
-    process2 = Process(target=check_ticket, args=(tickets_2,))
-    process3 = Process(target=check_ticket, args=(tickets_3,))
-    process4 = Process(target=check_ticket, args=(tickets_4,))
+    # process1 = Process(target=check_ticket(tickets_1), args=(1,))
+    # process2 = Process(target=check_ticket(tickets_2), args=(1,))
+    # process3 = Process(target=check_ticket(tickets_3), args=(1,))
+    # process4 = Process(target=check_ticket(tickets_4), args=(1,))
 
     logging.info("Main: before running thread/process")
     beginning_time = time.time()
     print(f"Begin threads {beginning_time}")
 
-    # thread1.start()
-    # thread2.start()
-    # thread3.start()
-    # thread4.start()
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
 
-    process1.start()
-    process2.start()
-    process3.start()
-    process4.start()
+    # process1.start()
+    # process2.start()
+    # process3.start()
+    # process4.start()
 
     logging.info("Main: wait for the thread/process to finish")
 
-    # thread1.join()
-    # thread2.join()
-    # thread3.join()
-    # thread4.join()
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    thread4.join()
 
-    process1.join()
-    process2.join()
-    process3.join()
-    process4.join()
+    # process1.join()
+    # process2.join()
+    # process3.join()
+    # process4.join()
 
     ending_time = time.time()
     print(f"End time {ending_time}")
     logging.info("Main: all done")
-
-    print("Total lucky tickets: ", total_tickets)
     print(f"Spent time: {ending_time - beginning_time}")
+
+    # for th
+    print("Total lucky tickets th: ", total_tickets_th)
+
+    # # for mp
+    # total_tickets_mp = 0
+    # while not queue.empty():
+    #     total_tickets_mp += queue.get()
+    # print("Total lucky tickets mp: ", total_tickets_mp)
